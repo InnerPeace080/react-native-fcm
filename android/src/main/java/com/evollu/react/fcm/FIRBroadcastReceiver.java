358dc1d133923da2d32dd7958befd3d418469b11
tranphuong.080 <tranphuong.080@gmail.com>
2019-07-05 16:37:48 +0700

    use start forcground service

--- android/src/main/java/com/evollu/react/fcm/FIRBroadcastReceiver.java
+++ android/src/main/java/com/evollu/react/fcm/FIRBroadcastReceiver.java
@@ -2,6 +2,7 @@ package com.evollu.react.fcm;
 
 import android.content.BroadcastReceiver;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -16,6 +17,11 @@ public class FIRBroadcastReceiver extends BroadcastReceiver {
 
         Intent newIntent = new Intent(context, BackgroundService.class);
         newIntent.putExtras(intent.getExtras());
-        context.startService(newIntent);
+//        context.startService(newIntent);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(newIntent);
+        } else {
+            context.startService(newIntent);
+        }
     }
 }
